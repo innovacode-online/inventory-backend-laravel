eecc2919fe540e88d965f01ce6387d8d6401f0d5
Ale <duartemattias4@gmail.com>
2023-10-20 21:33:56 -0400

    adding product model and relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
@@ -15,4 +16,9 @@ class Category extends Model
         'slug',
     ];
 
+    // relaciones
+    public function products(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
